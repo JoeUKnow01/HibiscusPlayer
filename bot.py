3a3af4130741bc98232cbf9b3b5d82bb358f09ae
Joseph N Pauls <josephthezombie@gmail.com>
2025-01-22 16:55:43 -0500

    More barebones updates
    simply getting the bot to read messages and assigning it the prefix "h!" for all future commands

--- bot.py
+++ bot.py
@@ -3,16 +3,22 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.default()
 load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_SERVER') # Discord Servers are actually called Guilds
 
 client = discord.Client(intents = intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+bot = commands.Bot(command_prefix = 'h!' , intents=intents) # h! is bot command prefix for any bot actions
 
-client.run(TOKEN)
+
+@bot.command(name="test", help = "Responds with a simple message to test bot functionality")
+async def test(ctx):
+    response = "the test is working."
+    await ctx.send(response)
+bot.run(TOKEN)
