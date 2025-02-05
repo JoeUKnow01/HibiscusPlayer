2ff3734d77755da89ddb8d8f3388d3160b244a37
Joseph N Pauls <josephthezombie@gmail.com>
2025-02-04 20:06:45 -0500

    Closer to playing tracks
    -updated to python 3.12
    -updated wavelink
    -player now connects to lavalink server, playing in progress

--- bot.py
+++ bot.py
@@ -1,64 +1,81 @@
 # bot.py
 import os
-
+import logging
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
 import wavelink
 
-
 intents = discord.Intents.all()
 intents.default()
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_SERVER') # Discord Servers are actually called Guilds
+GUILD = os.getenv('DISCORD_SERVER')  # Discord Servers are actually called Guilds
 
-client = discord.Client(intents = intents)
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix = 'h!' , intents=intents) # h! is bot command prefix for any bot actions
+bot = commands.Bot(command_prefix='h!',
+				   intents=intents)  # h! is bot command prefix for any bot actions
+logging.basicConfig(level=logging.INFO)
 
 @bot.event
 async def on_ready():
-    print("HibiscusBot is online!")
-    bot.loop.create_task(node_connect())
+	logging.info("HibiscusBot is online!")
+	bot.loop.create_task(node_connect())
+
 
 @bot.event
-async def on_wavelink_node_ready(node: wavelink.Node):
-    print(f"Node {node.identifier} is ready! ")
+async def on_wavelink_node_ready(payload: wavelink.NodeReadyEventPayload):
+	logging.info(f"Node {payload.node} is ready!")
+
 
 async def node_connect():
-    await bot.wait_until_ready()
-    await wavelink.NodePool.create_node( bot=bot, #figure out why connection failure
-                                         identifier="Catfein DE",
-                                         host="lavalink.alfari.id",
-                                         port=443,
-                                         password="catfein",
-                                         https=True)
-#Lavalink node from https://lavalink.appujet.site/ssl
-#{
- # "identifier": "Catfein DE",
- # "password": "catfein",
- # "host": "lavalink.alfari.id",
- # "port": 443,
- # "secure": true
-#}
+	await bot.wait_until_ready()
+	node: wavelink.Node = wavelink.Node(client=bot,  # figure out why connection failure
+										identifier="Public Lavalink v4",
+										uri="wss://lava-v4.ajieblogs.eu.org:443", # wss means secure
+										password="https://dsc.gg/ajidevserver")
 
-@bot.command(name="test", help = "Responds with a simple message to test bot functionality")
+	await wavelink.Pool.connect(client=bot, nodes=[node])
+
+# Lavalink node from https://lavalink.appujet.site/ssl
+# {
+#  "identifier": "Public Lavalink v4",
+#  "password": "https://dsc.gg/ajidevserver",
+#  "host": "lava-v4.ajieblogs.eu.org",
+#  "port": 443,
+#  "secure": true
+# }
+
+
+@bot.command(name="test", help="Responds with a simple message to test bot functionality")
 async def test(ctx):
-    response = "the test is working."
-    await ctx.send(response)
+	response = "the test is working."
+	await ctx.send(response)
 
-@bot.command(name="play", help= "requests a song to play")
-async def play(ctx: commands.Context, *, search: wavelink.YouTubeTrack):
-    if not ctx.voice_client:
-        vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-    elif not ctx.author.voice_client:
-        return await ctx.send("You must be connected to a voice channel to play music.")
-    else:
-        vc: wavelink.Player = ctx.voice_client
 
-    vc.play(search)
+@bot.command(name="play", help="requests a song to play")
+async def play(ctx: commands.Context, *, search: str):  # playable searches all sources
+	if not ctx.voice_client:
+		vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+	elif not ctx.author.voice:
+		return await ctx.send("You must be connected to a voice channel to play music.")
+	
+		# Search for the track
+	try:
+		tracks: list[wavelink.player] = await wavelink.player.search(search)
+		if not tracks:
+			return await ctx.send("No tracks found.")
+
+		# Play the first track from the search results
+		track = tracks[0]
+		await vc.play(track)
+		await ctx.send(f"Now playing: **{track.title}** by **{track.author}**")
+	except Exception as e:
+		logging.error(f"Error playing track: {e}")
+		await ctx.send("An error occurred while trying to play the track.")
+
 
 bot.run(TOKEN)
